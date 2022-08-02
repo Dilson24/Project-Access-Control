acd5ccd08607038e76e6fe23b596746c880aef4b
Dilson_Cruz <80017136+Dilson24@users.noreply.github.com>
2022-08-02 15:47:39 -0500

    Add files via upload

---
+++ controlador/js/dinamic_styles.js
@@ -0,0 +1,12 @@
+const $from__select = document.getElementById("from__select");
+console.log($from__select);
+
+$from__select.style.border = "none";
+$from__select.style.background = "none";
+$from__select.style.color = "#FF8138";
+$from__select.style.padding = ".6em .3em";
+$from__select.style.outline = "none";
+$from__select.style.borderBottom = "1px solid #FF8138";
+$from__select.style.fontFamily = "Poppins, sans-sefif";
+
+
